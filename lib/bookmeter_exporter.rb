4cef9e16b6f2e68264540fc9cf8736d96408dae6
Ikuo Degawa <degawa@ikuwow.com>
2021-07-24 11:44:59 +0900

    Initial commit
    $ bundle gem bookmeter_exporter

---
+++ lib/bookmeter_exporter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bookmeter_exporter/version"
+
+module BookmeterExporter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/bookmeter_exporter_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BookmeterExporter do
+  it "has a version number" do
+    expect(BookmeterExporter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
